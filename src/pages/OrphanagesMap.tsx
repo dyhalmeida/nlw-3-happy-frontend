a12cc63e189b7470391f3e5a3d1b9361bd01d4fd
Diego Almeida <dyhalmeida@gmail.com>
2020-10-14 14:40:50 -0300

    Adicionado marker no mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import leaflet from 'leaflet';
 import markerSVG from '../assets/marker.svg';
 
 import '../styles/pages/orphanagesMap.css';
 import 'leaflet/dist/leaflet.css';
+
+const mapMarker = leaflet.icon({
+  iconUrl: markerSVG,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68], // Posição x,y do icon que representa o ponto
+  popupAnchor: [170, 2],
+});
 
 const OrphanagesMap = () => {
   return (
@@ -28,6 +36,19 @@ const OrphanagesMap = () => {
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[-12.5443141, -38.2954662]} icon={mapMarker}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to="">
+              <FiArrowRight size={20} color="#ffffff" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Link to="" className="create-orphanage">
